bbda51e5fc8f5981bf48bf39d7ea3c6448caa7d3
PolCarCat <polcarcat@gmail.com>
2018-09-27 22:24:26 +0200

    Projectrion matrix fixed

--- 3DEngine/ModuleRenderer3D.cpp
+++ 3DEngine/ModuleRenderer3D.cpp
@@ -76,8 +76,8 @@ bool ModuleRenderer3D::Init()
 		}
 
 		//Initialize Modelview Matrix
-		//glMatrixMode(GL_MODELVIEW);
-		//glLoadIdentity();
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
 
 		//Check for error
 		error = glGetError();
@@ -181,8 +181,8 @@ void ModuleRenderer3D::OnResize(int width, int height)
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	//ProjectionMatrix = perspective(60.0f, (float)width / (float)height, 0.125f, 512.0f);
-	//glLoadMatrixf(&ProjectionMatrix);
+	ProjectionMatrix = perspective(60.0f, (float)width / (float)height, 0.125f, 512.0f);
+	glLoadMatrixf(&ProjectionMatrix);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
