d07f6c1a899b5ec8c6441040856c0e7d659db07f
retsnom9 <retsnom797@gmail.com>
2018-10-25 08:32:41 +0200

    Created ImporterMesh Class

---
+++ 3DEngine/ImporterMesh.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Importer.h"
+#include "Globals.h"
+
+#define CHECKERS_SIZE 20
+
+
+class ImporterMesh :
+	public Importer
+{
+public:
+	ImporterMesh();
+	~ImporterMesh();
+
+	bool Load();
+	bool Save();
+	bool Start();
+	bool CleanUp();
+
+};
